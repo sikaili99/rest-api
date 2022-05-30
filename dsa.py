5ee39165881c1ac764a7fe519d85c055592f04a9
Mathewsmusukuma <sikaili99@gmail.com>
2022-05-31 01:08:59 +0200

    Delete file

--- dsa.py
+++
@@ -1,9 +0,0 @@
-def squense(array,squense):
-    squenseIdx = 0
-
-    for n in array:
-        if squenseIdx == len(squense):
-            break 
-        if n == squense[squenseIdx]:
-            squenseIdx += 1
-    return squenseIdx == len(squense)
